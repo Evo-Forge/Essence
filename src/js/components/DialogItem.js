f1082c6325a3410edeaea102b03578122d53be6f
Hetmann W. Iohan <wilhelm.hetmann@evozon.com>
2015-06-19 15:52:51 +0300

    Essence - DialogItem onClosing option

--- src/js/components/DialogItem.js
+++ src/js/components/DialogItem.js
@@ -85,7 +85,7 @@ module.exports = React.createClass({
 
         document.querySelector('body').className = '';
       }
-      
+
       if (this.props.onClosing) {
         return self.props.onClosing();
       }
