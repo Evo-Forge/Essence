4c2203ba838ff1caa4fe4c6d1c2bacc72dac5b98
Hetmann W. Iohan <wilhelm.hetmann@evozon.com>
2015-11-27 11:50:34 +0200

    Chips - on hover bgcolor: project template restored content

--- examples/project-template/dist/js/project.js
+++ examples/project-template/dist/js/project.js
@@ -68,29 +68,7 @@ var body = document.body, // default React render path is html body
 
 ReactDOM.render(
     <Block type='div' classes='e-background-grey-500 e-text-center'>
-      <Chip>
-  <ChipItem
-    image='assets/img/card-user-img.jpg'
-    imageAlt='Card Image'
-    name='Gonzales'
-    email='gon@zal.es'
-  >
-  </ChipItem>
-  <ChipItem
-    image='assets/img/card-user-img.jpg'
-    imageAlt='Card Image'
-    name='Iolanda Curtiz'
-    email='io@lan.da'
-  >
-  </ChipItem>
-  <ChipItem
-    image='assets/img/card-user-img.jpg'
-    imageAlt='Card Image'
-    name='Gizela Timliv'
-    email='gizela@timl.iv'
-  >
-  </ChipItem>
-</Chip>
+      your content by ID # here
     </Block>
     ,byID
 );
