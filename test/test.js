4eba4e785efefb90e7108ba91c1ef6e3bcd2a196
Dan Birle <dan.birle@evozon.com>
2016-01-14 13:31:03 +0300

    appbar component - refactoring

--- test/test.js
+++ test/test.js
@@ -6,27 +6,33 @@ import Text from '../components/essence-core/src/text/text.jsx';
 import Block from '../components/essence-core/src/block/block.jsx';
 import Icon from '../components/essence-icon/src/icon.jsx';
 import Menu from '../components/essence-menu/src/menu.jsx';
+import List from '../components/essence-list/src/list.jsx';
+import ListItem from '../components/essence-list/src/item.jsx';
+import Navigation from '../components/essence-navigation/src/navigation.jsx';
 
 ReactDOM.render(
+
 	<AppBar classes={'e-background-yellow-100'}>
-		
-		<Icon name={'navigation-menu'} classes={'e-left'}/>
-
-		<Menu type={'cover'} classes={'e-left'} placeholder='Options'>			
+		<Icon name={'navigation-menu'} classes={'e-left'} />
+	    <Menu type={'cover'} classes={'e-left'} placeholder={'Options'}>			
+	    	<Block>
+	    		<Icon name='notification-sms' />
+				<Text>Notifications</Text>				
+			</Block>
 			<Block>
-				<Icon name='notification-sms' />
-				<Text>Notifications</Text>
+	    		<Icon name='notification-tap-and-play' />
+				<Text>Tap & Play</Text>				
+			</Block>
+			<Block>
+	    		<Icon name='notification-vibration' />
+				<Text>Vibrations</Text>				
 			</Block>
 
-			<Block>
-				<Icon name='notification-tap-and-play' />
-				<Text>Tap & Pay</Text>
-			</Block>
 
-			<Block classes={'divider'} />
-
-			<Text type={'a'} href='http://www.google.com/' target={'_target'}>Google</Text>
 		</Menu>
+	    <Icon name={'navigation-more-vert'} classes={'e-right'} />
+	    <Icon name={'action-favorite'} classes={'e-right'} />
+	    <Icon name={'action-search'} classes={'e-right'} />	    
 	</AppBar>
 	,
 	document.querySelector('.app')
