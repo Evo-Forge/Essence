363bee644c962733783a1c9ae5dbb49f59f99cbc
Dan Birle <dan.birle@evozon.com>
2016-01-19 16:59:55 +0300

    navigation-drawer; position e-nav-drawer

--- test/test.js
+++ test/test.js
@@ -20,27 +20,7 @@ import DataTableColumn from '../components/essence-data-table/src/column.jsx';
 ReactDOM.render(
 
 	<Block>
-		<AppBar classes={'e-background-yellow-100'}>
-			<Icon name={'navigation-menu'} classes={'e-left'} />
-			<Menu type={'cover'} classes={'e-left'} placeholder={'Options'}>
-			   	<Block placeholder={'Notifications'}>
-				   	<Icon name='notification-sms' />
-					<Text>Notifications</Text>
-				</Block>
-				<Block placeholder={'Tap & Play'}>
-				   	<Icon name='notification-tap-and-play' />
-					<Text>Tap & Play</Text>
-				</Block>
-				<Block>
-				   	<Icon name='notification-vibration' />
-					<Text>Vibrations</Text>
-				</Block>
-			</Menu>
-		    <Icon name={'navigation-more-vert'} classes={'e-right'} />
-		    <Icon name={'action-favorite'} classes={'e-right'} />
-		    <Icon name={'action-search'} classes={'e-right'} />	   
-		</AppBar>
-{/*
+
 		<Navigation visible={true} classes={'e-background-green-100'}>
 			<Block classes={'e-navigation-wrapper'} id={'navigationMenu'}>
 				<Block type={'header'} classes={'e-nav-header e-background-red-100'}>
@@ -78,8 +58,28 @@ ReactDOM.render(
 				</Block>
 			</Block>
 		</Navigation>
-*/}
 
+		
+		<AppBar classes={'e-background-yellow-100'}>
+			<Icon name={'navigation-menu'} classes={'e-left'} />
+			<Menu type={'cover'} classes={'e-left'} placeholder={'Options'}>
+			   	<Block placeholder={'Notifications'}>
+				   	<Icon name='notification-sms' />
+					<Text>Notifications</Text>
+				</Block>
+				<Block placeholder={'Tap & Play'}>
+				   	<Icon name='notification-tap-and-play' />
+					<Text>Tap & Play</Text>
+				</Block>
+				<Block>
+				   	<Icon name='notification-vibration' />
+					<Text>Vibrations</Text>
+				</Block>
+			</Menu>
+		    <Icon name={'navigation-more-vert'} classes={'e-right'} />
+		    <Icon name={'action-favorite'} classes={'e-right'} />
+		    <Icon name={'action-search'} classes={'e-right'} />	   
+		</AppBar>
 
 		<Block classes={'e-row e-margin-top-25'}>
 			<Block classes={'brick brick-4'}></Block>
@@ -181,7 +181,9 @@ ReactDOM.render(
 			</DataTableBody>
 		</DataTable>
 
+
 	</Block>
+
 
 	,
 	document.querySelector('.app')
