b145a64540744173ffb3ab18683f4fb95c766d9a
Hetmann W. Iohan <wilhelm.hetmann@evozon.com>
2016-01-19 16:55:17 +0200

    Data Table - check row

--- components/essence-data-table/src/table.jsx
+++ components/essence-data-table/src/table.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ClassNames from 'classnames';
+import Switch from '../../essence-switch/src/switch.jsx';
+
+import DataTableRow from './row.jsx';
+import DataTableBody from './body.jsx';
+import DataTableHeader from './header.jsx';
+import DataTableColumn from './column.jsx';
+
 import './table.less';
  
 class DataTable extends React.Component {
@@ -11,14 +18,138 @@ class DataTable extends React.Component {
                 'datatable',
                 this.props.classes,
                 this.props.className
-            )
+            ),
+            selectedRows: {},
+            selectedAllRows: false
         };
+    }
+
+    componentDidMount() {
+        let self = this,
+            selected = {},
+            dataObj = this.props.data;
+
+        if (dataObj.rows) {
+            (dataObj.rows).map(function(row, rowIndex){
+                selected['row-'+rowIndex] = false;
+            });
+        }
+
+        this.setState({
+            selectedRows: selected
+        });
+    }
+
+    checkRows() {
+        let selectedRows = this.state.selectedRows,
+            selectedAllRows = this.state.selectedAllRows;
+
+        for (var rowIndex in selectedRows) {
+            selectedRows[rowIndex] = !selectedAllRows;
+        };
+
+        this.setState({
+            selectedRows: selectedRows,
+            selectedAllRows: !selectedAllRows
+        });
+
+        return this;
+    }
+
+    checkRow(rowIndex, row) {
+        let selectedRows = this.state.selectedRows;
+        
+        selectedRows['row-'+rowIndex] = !selectedRows['row-'+rowIndex];
+
+        this.setState({
+            selectedRows: selectedRows
+        });
+
+        return this;
+    }
+
+    renderHeader() {
+        let self = this,
+            dataObj = this.props.data;
+
+        if (dataObj.header) {
+            var rows = (dataObj.header).map(function(arr, index){ 
+                return (<DataTableColumn key={index}>{arr.name}</DataTableColumn>);
+            });
+
+            return (
+                <DataTableHeader classes={'e-text-grey-400'} key={'header'}>
+                    <DataTableRow>
+                    {[
+                        <DataTableColumn key={'checkall'}>
+                            <Switch 
+                                type='checkbox' 
+                                name='checkall'
+                                onClick={this.checkRows.bind(self)} />
+                        </DataTableColumn>,
+                        rows
+                    ]}
+                    </DataTableRow>
+                </DataTableHeader>
+            );
+        }
+    }
+
+    renderRows() {
+        let self = this,
+            selectedRows = this.state.selectedRows,
+            dataObj = this.props.data;
+
+        if (dataObj.rows) {
+            return (
+               <DataTableBody classes={'e-text-grey-700'} key={'body'}>
+                {
+                    dataObj.rows.map(function(row, rowIndex){
+                        return (
+                            <DataTableRow 
+                                key={'row-'+rowIndex} 
+                                ref={'row-'+rowIndex} 
+                                selected={selectedRows['row-'+rowIndex]}
+                            >
+                            {[
+                                <DataTableColumn key={'check-'+rowIndex}>
+                                    <Switch 
+                                        type='checkbox' 
+                                        name={'check-'+rowIndex}
+                                        checked={selectedRows['row-'+rowIndex]}
+                                        onClick={this.checkRow.bind(this, rowIndex)} />
+                                </DataTableColumn>,
+                                row.map(function(arr, arrIndex){
+                                    return (
+                                        <DataTableColumn key={rowIndex + arrIndex}>
+                                            {arr}
+                                        </DataTableColumn>
+                                    )
+                                }, this)
+                            ]}
+                            </DataTableRow>
+                        );
+                    }, this)
+                }
+                </DataTableBody>);
+        }
+        return '';
+    }
+
+    renderChildren() {
+        let data = this.props.data;
+
+        if (data) {
+            return [this.renderHeader(), this.renderRows()];
+        }
+
+        return this.props.children;
     }
 
     render() {
         return (
             <table className={this.state.classes} ref={(ref) => this.dataTable = ref}>
-                {this.props.children}
+                {this.renderChildren()}
             </table>
         );
     }
--- test/test.js
+++ test/test.js
@@ -5,120 +5,45 @@ import AppBar from '../components/essence-appbar/src/appbar.jsx';
 import Text from '../components/essence-core/src/text/text.jsx';
 import Block from '../components/essence-core/src/block/block.jsx';
 import Icon from '../components/essence-icon/src/icon.jsx';
-import Menu from '../components/essence-menu/src/menu.jsx';
 import Switch from '../components/essence-switch/src/switch.jsx';
-import List from '../components/essence-list/src/list.jsx';
-import ListItem from '../components/essence-list/src/item.jsx';
-import Navigation from '../components/essence-navigation/src/navigation.jsx';
-import Image from '../components/essence-image/src/image.jsx';
+import Input from '../components/essence-input/src/input.jsx';
+
 import DataTable from '../components/essence-data-table/src/table.jsx';
 import DataTableHeader from '../components/essence-data-table/src/header.jsx';
 import DataTableBody from '../components/essence-data-table/src/body.jsx';
 import DataTableRow from '../components/essence-data-table/src/row.jsx';
 import DataTableColumn from '../components/essence-data-table/src/column.jsx';
 
+var tableData = {
+	'header': [ 
+	    {'name': 'Desert (100g serving)', 'tooltip': 'Caption for the column Desert (100g serving)'},
+	    {'name': 'Calories', 'tooltip': 'Caption for the column Calories'},
+	    {'name': 'Fat (g)', 'tooltip': 'Caption for the column Fat (g)'},
+	    {'name': 'Carbs (g)', 'tooltip': 'Caption for the column Carbs (g)'},
+	    {'name': 'Protein (g)', 'tooltip': 'Caption for the column Protein (g)'},
+	    {'name': 'Sodium (mg)', 'tooltip': 'Caption for the column Sodium (mg)'},
+	    {'name': 'Calcium (%)', 'tooltip': 'Caption for the column Calcium (%)'},
+	    {'name': 'Iron (%)', 'tooltip': 'Caption for the column Iron (%)'},
+	],
+	'rows': [
+		['Frozen yogurt', '159', '6.0', '24', '4.0', '87', '14%', '1%'],
+		['Ice cream sandwich', '237', '9.0', '37', '4.3', '129', '8%', '1%'],
+		['Eclair', '262', '16.0', '24', '6.0', '337', '6%', '7%'],
+		['Cupcake', '305', '16.0', '24', '6.0', '413', '3%', '6%'],
+		['Gingerbread', '356', '3.7', '67', '4.3', '413', '3%', '8%'],
+		['Jelly bean', '356', '16.0', '94', '4.3', '129', '8%', '16%'],
+		['Lollipop', '392', '0.2', '97', '0.0', '50', '0%', '2%'],
+		['Honeycomb', '408', '3.2', '87', '0.0', '129', '4%', '45%'],
+		['Donut', '452', '25.0', '51', '4.9', '326', '0%', '22%'],
+		['KitKat', '518', '26.0', '65', '7.0', '54', '8%', '6%'],
+		['Input', '518', '26.0', '65', '7.0', '54', '8%', (<Input name='input' placeholder='Carja input' classes={'has-success'} />)]
+	]
+};
+
 ReactDOM.render(
-
-	<Block>
-		<AppBar classes={'e-background-yellow-100'}>
-			<Icon name={'navigation-menu'} classes={'e-left'} />
-			<Menu type={'cover'} classes={'e-left'} placeholder={'Options'}>
-			   	<Block placeholder={'Notifications'}>
-				   	<Icon name='notification-sms' />
-					<Text>Notifications</Text>
-				</Block>
-				<Block placeholder={'Tap & Play'}>
-				   	<Icon name='notification-tap-and-play' />
-					<Text>Tap & Play</Text>
-				</Block>
-				<Block>
-				   	<Icon name='notification-vibration' />
-					<Text>Vibrations</Text>
-				</Block>
-			</Menu>
-		    <Icon name={'navigation-more-vert'} classes={'e-right'} />
-		    <Icon name={'action-favorite'} classes={'e-right'} />
-		    <Icon name={'action-search'} classes={'e-right'} />	   
-		</AppBar>
-{/*
-		<Navigation visible={true} classes={'e-background-green-100'}>
-			<Block classes={'e-navigation-wrapper'} id={'navigationMenu'}>
-				<Block type={'header'} classes={'e-nav-header e-background-red-100'}>
-					<Text>Navigation header</Text>
-				</Block>
-				<List type={'navigation'}>
-					<ListItem>
-						<Text type={'a'} href={'#inbox'}>
-							<Icon name={'content-inbox'} />
-							Inbox
-						</Text>
-					</ListItem>
-					<ListItem>
-						<Text type={'a'} href={'#starred'}>
-							<Icon name={'action-grade'} />
-							Starred
-						</Text>
-					</ListItem>
-					<ListItem>
-						<Text type={'a'} href={'#send'}>
-							<Icon name={'content-send'} />
-							Sent mail
-						</Text>
-					</ListItem>
-					<ListItem>
-						<Text type={'a'} href={'#item 4'}>
-							<Icon name={'action-account-circle'} />
-							Drafts
-						</Text>
-					</ListItem>
-					
-				</List>
-				<Block type={'footer'} classes={'e-nav-footer e-background-red-100'}>
-					Navigation footer
-				</Block>
-			</Block>
-		</Navigation>
-*/}
-
-
-		<Block classes={'e-row e-margin-top-25'}>
-			<Block classes={'brick brick-4'}></Block>
-			<Block classes={'brick brick-4 e-shadow-1 e-no-padding'}>
-				<List type={'navigation'}>
-					<ListItem>
-						<Text type={'a'} href={'#inbox'}>	
-							<Image classes={'e-avatar e-left'} src='http://i.imgur.com/3pmiEzi.jpg' alt={'Boat'} />
-							Inbox
-							<Icon name={'content-inbox e-right'} />
-						</Text>
-					</ListItem>
-					<ListItem>
-						<Text type={'a'} href={'#starred'}>
-							<Image classes={'e-avatar e-left'} src='http://i.imgur.com/3pmiEzi.jpg' alt={'Boat'} />
-							Starred
-							<Icon name={'action-grade e-right'} />	
-						</Text>
-					</ListItem>
-					<ListItem>
-						<Text type={'a'} href={'#send'}>
-							<Image classes={'e-avatar e-left'} src='http://i.imgur.com/3pmiEzi.jpg' alt={'Boat'} />
-							Sent mail
-							<Icon name={'content-send e-right'} />
-						</Text>
-					</ListItem>
-					<ListItem>
-						<Text type={'a'} href={'#send'}>
-							<Image classes={'e-avatar e-left'} src='http://i.imgur.com/3pmiEzi.jpg' alt={'Boat'} />
-							Drafts
-							<Icon name={'action-account-circle e-right'} />
-						</Text>
-					</ListItem>
-				</List>
-			</Block>
-			<Block classes={'brick brick-4'}></Block>
-		</Block>
-
-
+	<Block classes={'brick brick-12'}>
+		<DataTable data={tableData} />
+		{/*
 		<DataTable>
 			<DataTableHeader classes={'e-text-grey-400'}>
 				<DataTableRow>
@@ -180,7 +105,7 @@ ReactDOM.render(
 				</DataTableRow>
 			</DataTableBody>
 		</DataTable>
-
+		*/}
 	</Block>
 
 	,
