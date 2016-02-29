885390e62a9b7846154390579cb5cc09e5e027d1
Hetmann W. Iohan <wilhelm.hetmann@evozon.com>
2016-02-29 18:50:37 +0200

    Slider - default value from start props

--- components/essence-slider/src/slider.jsx
+++ components/essence-slider/src/slider.jsx
@@ -7,17 +7,14 @@ class Slider extends React.Component {
         super(props);
         this.state = {
         	classes: ClassNames(
-                {
-                    'discrete': (this.props.discrete ? true : false),
-                    'zero': (parseInt(this.props.start) > 1 ? false : true)
-                },
                 this.props.className,
                 this.props.classes
             ),
+            start: props.start || 0,
             lowerColor: props.lowerColor || 'e-background-indigo-400',
-            lowerWidth: this.props.start ? this.props.start+'%' : '0%',
+            lowerWidth: props.start ? props.start+'%' : '0%',
             upperColor: props.upperColor || 'e-background-grey-100',
-            upperWidth: this.props.start ? 100 - this.props.start+'%' : '100%'
+            upperWidth: props.start ? 100 - props.start+'%' : '100%'
         };
     }
 
@@ -51,7 +48,7 @@ class Slider extends React.Component {
 	render() {
         return (
             <div className={'e-slider'}>
-                <input type='range' {...this.props} ref={(ref) => this.sliderInput = ref} defaultValue={this.state.inputValue} />
+                <input type='range' {...this.props} ref={(ref) => this.sliderInput = ref} defaultValue={this.state.start} />
                 <div className={'background'}>
                     <div className={ClassNames('lower', this.state.lowerColor)} style={{width: this.state.lowerWidth}} />
                     <div className={ClassNames('upper', this.state.upperColor)} style={{width: this.state.upperWidth}} />
