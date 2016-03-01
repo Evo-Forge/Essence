b75a0021e86be38c9396d1fc787911eac568f0c0
Dan Birle <dan.birle@evozon.com>
2016-03-01 10:51:01 +0300

    slider mdl

--- components/essence-slider/src/slider.jsx
+++ components/essence-slider/src/slider.jsx
@@ -12,9 +12,9 @@ class Slider extends React.Component {
             ),
             start: props.start || 0,
             lowerColor: props.lowerColor || 'e-background-indigo-400',
-            lowerWidth: props.start ? props.start+'%' : '0%',
+            lowerFlex: props.start ? (props.start / 100)+' 1 0%' : '0 1 0%',
             upperColor: props.upperColor || 'e-background-grey-100',
-            upperWidth: props.start ? 100 - props.start+'%' : '100%'
+            upperFlex: props.start ? 1 - (props.start / 100)+' 1 0%' : '1 1 0%'
         };
     }
 
@@ -24,8 +24,8 @@ class Slider extends React.Component {
             let sliderValue = self.sliderInput.value;
 
             self.setState({
-                lowerWidth: sliderValue+'%',
-                upperWidth: 100 - sliderValue+'%'
+                lowerFlex: (sliderValue / 100)+' 1 0%',
+                upperFlex: 1 - (sliderValue / 100)+' 1 0%'
             });
         }, false);      
     }
@@ -50,8 +50,8 @@ class Slider extends React.Component {
             <div className={'e-slider'}>
                 <input type='range' {...this.props} ref={(ref) => this.sliderInput = ref} defaultValue={this.state.start} />
                 <div className={'track'}>
-                    <div className={ClassNames('left', this.state.lowerColor)} style={{width: this.state.lowerWidth}} />
-                    <div className={ClassNames('right', this.state.upperColor)} style={{width: this.state.upperWidth}} />
+                    <div className={ClassNames('left', this.state.lowerColor)} style={{flex: this.state.lowerFlex}} />
+                    <div className={ClassNames('right', this.state.upperColor)} style={{flex: this.state.upperFlex}} />
                 </div>
             </div>
         );
