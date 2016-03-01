ac1923a6957dc0095dfff7c6769041853d50802f
Hetmann W. Iohan <wilhelm.hetmann@evozon.com>
2016-03-01 14:37:14 +0200

    Slider - max & calculate flex based on max value

--- components/essence-slider/src/slider.jsx
+++ components/essence-slider/src/slider.jsx
@@ -15,9 +15,9 @@ class Slider extends React.Component {
             start: props.start || 0,
             dataValue: props.start || 0,
             lowerColor: props.lowerColor || 'e-background-indigo-400',
-            lowerFlex: props.start ? (props.start / 100)+' 1 0%' : '0 1 0%',
+            lowerFlex: props.start ? (props.start / (props.max || 100))+' 1 0%' : '0 1 0%',
             upperColor: props.upperColor || 'e-background-grey-100',
-            upperFlex: props.start ? 1 - (props.start / 100)+' 1 0%' : '1 1 0%'
+            upperFlex: props.start ? 1 - (props.start / (props.max || 100))+' 1 0%' : '1 1 0%'
         };
     }
 
@@ -44,8 +44,8 @@ class Slider extends React.Component {
             self.setState({
                 dataValue: sliderValue,
                 zeroActive: sliderValue < 1 ? true : false,
-                lowerFlex: (sliderValue / 100)+' 1 0%',
-                upperFlex: 1 - (sliderValue / 100)+' 1 0%'
+                lowerFlex: (sliderValue / (self.props.max || 100))+' 1 0%',
+                upperFlex: 1 - (sliderValue / (self.props.max || 100))+' 1 0%'
             });
         }, false);
 
@@ -58,7 +58,13 @@ class Slider extends React.Component {
                 {'zero': this.state.zeroActive},
                 {'disabled': this.props.disabled}
             )}>
-                <input type='range' {...this.props} ref={(ref) => this.sliderInput = ref} defaultValue={this.state.dataValue} data-discrete={this.state.dataValue} />
+                <input 
+                    type='range'
+                    {...this.props} 
+                    ref={(ref) => this.sliderInput = ref} 
+                    defaultValue={this.state.dataValue} 
+                    data-discrete={this.state.dataValue} 
+                />
                 <div className={'track'}>
                     <div className={ClassNames('left', this.state.lowerColor)} style={{flex: this.state.lowerFlex}} />
                     <div className={ClassNames('right', this.state.upperColor)} style={{flex: this.state.upperFlex}} />
